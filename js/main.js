11309f410b2a456af29e47332425810868486dc5
Duskaryis <derekpayne1991@hotmail.com>
2025-05-05 16:35:10 -0400

    working on theme toggle

--- js/main.js
+++ js/main.js
@@ -1,8 +1,62 @@
+const theme = 'theme';
+const dataTheme = 'data-theme';
+const themeTab = '.theme-tab';
+const switcherBtn = '.switcher-btn';
+const dark = 'dark';
+const light = 'light';
+const open = 'open';
+const active = 'active';
+
 const modalOpen = '[data-open]';
 const modalClose = '[data-close]';
 const isVisible = 'is-visible';
+
+const root = document.documentElement;
+
+/*Theme*/
+const toggleTheme = document.querySelector(themeTab);
+const switcher = document.querySelectorAll(switcherBtn);
+const currentTheme = localStorage.getItem(theme);
+
+/* Modal */
 const openModal = document.querySelectorAll(modalOpen);
 const closeModal = document.querySelectorAll(modalClose);
+
+const setActive = (elm, selector) => {
+	if (document.querySelector(`${selector}.${active}`) !== null) {
+		document.querySelector(`${selector}.${active}`).classList.remove(active);
+	} else {
+		elm.classList.add(active);
+	}
+};
+
+const setTheme = (val) => {
+	if (val === 'dark') {
+		root.setAttribute(dataTheme, dark);
+		localStorage.setItem(theme, dark);
+	} else {
+		root.setAttribute(dataTheme, light);
+		localStorage.setItem(theme, light);
+	}
+};
+
+toggleTheme.addEventListener('click', function () {
+	const tab = this.parentElement.parentElement;
+	if (!tab.className.includes(open)) {
+		tab.classList.add(open);
+	} else {
+		tab.classList.remove(open);
+	}
+});
+
+for (const elm of switcher) {
+	elm.addEventListener('click', function () {
+		const toggle = this.dataset.toggle;
+		// set active state
+		setActive(elm, switcherBtn);
+		setTheme(toggle);
+	});
+}
 
 // Full Site Modal "open buttons"
 for (const elm of openModal) {
